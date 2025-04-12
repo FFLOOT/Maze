7c02efad1b19c94a6a333d49daf122f68fdf7dad
malika-mk <malikamakkambay@gmail.com>
2025-04-12 23:16:06 +0600

    Hard Walls

--- Maze.py
+++ Maze.py
@@ -26,48 +26,69 @@ class Player(turtle.Turtle):
           self.speed(0)
 
      def go_up(self):
-          self.goto(self.xcor(), self.ycor() + 24)
+
+          move_to_x = player.xcor()
+          move_to_y = player.ycor() + 24
+
+
+          if (move_to_x, move_to_y) not in walls:
+               self.goto(move_to_x, move_to_y)
             
      def go_down(self):
-          self.goto(self.xcor(), self.ycor() - 24)
+          move_to_x = player.xcor()
+          move_to_y = player.ycor() - 24
+
+
+          if (move_to_x, move_to_y) not in walls:
+               self.goto(move_to_x, move_to_y)
                
      def go_left(self):
-          self.goto(self.xcor() -24, self.ycor())
+          move_to_x = player.xcor() - 24
+          move_to_y = player.ycor()
+
+
+          if (move_to_x, move_to_y) not in walls:
+               self.goto(move_to_x, move_to_y)
                
      def go_right(self):
-          self.goto(self.xcor() +24, self.ycor())
+          move_to_x = player.xcor() + 24
+          move_to_y = player.ycor()
+
+
+          if (move_to_x, move_to_y) not in walls:
+               self.goto(move_to_x, move_to_y)
 
 
 levels = [""]
 
 
 level_1 = [
-            "XXXXXXXXXXXXXXXXXXXXXXXXX",
-            "XP XXXXXXX            XXX",
-            "X  XXXXXXX   XXXXXX   XXX",
-            "X            XXXXXX   XXX",
-            "XXXXXXXXXX   XX       XXX",
-            "XXXXXXXXXX   XXXXXX   XXX",
-            "X                XXXXXXXX",
-            "X    XXXXXXXXX   XXXXXXXX",
-            "X    XXXXXXXXX          X",
-            "X                       X",
-            "X    XXXXXXXXXXXXXX     X",
-            "X    XXXXXXXXXXXXXX     X",
-            "X         XXXXXXXXX     X",
-            "XXXXX     XXXXX         X",
-            "XXXXX     XXXXX         X",
-            "XXXXX     XXXXX    XXXXXX",
-            "X         XXXXX    XXXXXX",
-            "X         XXXXXXXXXXXXXXX",
-            "XXXXX                XXXX",
-            "XXXXXX               XXXX",
-            "XXXXXXXXXXXXXXXXX    XXXX",
-            "XXXXXXXXXXXXXXXXX    XXXX",
-            "X     XXXXXXXXXXX    XXXX",
-            "X                       X",
-            "XXXXXXXXXXXXXXXXXXXXXXXXX",
-        ]
+"XXXXXXXXXXXXXXXXXXXXXXXXX",
+"XP XXXXXXX            XXX",
+"X  XXXXXXX   XXXXXX   XXX",
+"X            XXXXXX   XXX",
+"XXXXXXXXXX   XX       XXX",
+"XXXXXXXXXX   XXXXXX   XXX",
+"X                XXXXXXXX",
+"X    XXXXXXXXX   XXXXXXXX",
+"X    XXXXXXXXX          X",
+"X                       X",
+"X    XXXXXXXXXXXXXX     X",
+"X    XXXXXXXXXXXXXX     X",
+"X         XXXXXXXXX     X",
+"XXXXX     XXXXX         X",
+"XXXXX     XXXXX         X",
+"XXXXX     XXXXX    XXXXXX",
+"X         XXXXX    XXXXXX",
+"X         XXXXXXXXXXXXXXX",
+"XXXXX                XXXX",
+"XXXXXX               XXXX",
+"XXXXXXXXXXXXXXXXX    XXXX",
+"XXXXXXXXXXXXXXXXX    XXXX",
+"X     XXXXXXXXXXX    XXXX",
+"X                       X",
+"XXXXXXXXXXXXXXXXXXXXXXXXX",
+]
 
 
 levels.append(level_1)
@@ -88,6 +109,8 @@ def setup_maze(level):
                         pen.goto(screen_x, screen_y)
                         pen.stamp()
 
+                        walls.append((screen_x, screen_y))
+
 
                     if character == "P":
                              player.goto(screen_x, screen_y)
@@ -97,7 +120,11 @@ pen = Pen()
 player = Player()
 
 
+walls = []
+
+
 setup_maze(levels[1])
+print (walls)
 
 
 turtle.listen()
