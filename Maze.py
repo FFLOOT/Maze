5bca8fc4b680efab6144d9cc7be87c2b2841f6e9
malika-mk <malikamakkambay@gmail.com>
2025-04-13 16:31:17 +0600

    Enemies

--- Maze.py
+++ Maze.py
@@ -3,17 +3,20 @@
 
 import turtle
 import math
+import random
 
 wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("A Maze Game")
 wn.setup(700,700)
+wn.tracer(0)
 
 
-turtle.register_shape("Images/Girl_right.gif")
-turtle.register_shape("Images/Gir_left.gif")
-turtle.register_shape("Images/Sparkle.gif")
-turtle.register_shape("Images/Wall.gif")
+images = ["Images/Girl_right.gif", "Images/Gir_left.gif",
+          "Images/Sparkle.gif", "Images/Wall.gif",
+          "Images/Enemy_right.gif", "Images/Enemy_left.gif"]
+for image in images:
+     turtle.register_shape(image)
 
 
 class Pen(turtle.Turtle):
@@ -59,6 +62,7 @@ class Player(turtle.Turtle):
 
           self.shape("Images/Gir_left.gif")
 
+
           if (move_to_x, move_to_y) not in walls:
                self.goto(move_to_x, move_to_y)
 
@@ -69,6 +73,7 @@ class Player(turtle.Turtle):
           move_to_y = player.ycor()
           
           self.shape("Images/Girl_right.gif")
+
 
           if (move_to_x, move_to_y) not in walls:
                self.goto(move_to_x, move_to_y)
@@ -97,13 +102,61 @@ class Sparkle(turtle.Turtle):
           self.goto(2000, 2000)
           self.hideturtle()
 
+class Enemy(turtle.Turtle):
+     def __init__(self, x, y):
+          turtle.Turtle.__init__(self)
+          self.shape("Images/Enemy_left.gif")
+          self.color("blue")
+          self.penup()
+          self.speed(0)
+          self.blue = 25
+          self.goto(x, y)
+          self.direction = random.choice(["up", "down", "left", "right"])
+
+     def move(self):
+          if self.direction == "up":
+               dx = 0
+               dy = 24
+          elif self.direction == "down":
+               dx = 0
+               dy = -24
+          elif self.direction == "left":
+               dx = -24
+               dy = 0
+               self.shape("Images/Enemy_left.gif")
+          elif self.direction == "right":
+               dx = 24
+               dy = 0
+               self.shape("Images/Enemy_right.gif")
+          else:
+               dx = 0
+               dy = 0
+
+
+          move_to_x = self.xcor() + dx
+          move_to_y = self.ycor() + dy
+
+
+          if (move_to_x, move_to_y) not in walls:
+               self.goto(move_to_x, move_to_y)
+          else:
+
+               self.direction = random.choice(["up", "down", "left", "right"])
+
+
+          turtle.ontimer(self.move, t=random.randint(100, 300))
+
+     def destroy(self):
+          self.goto(2000, 2000)
+          self.hideturtle()
+
 
 levels = [""]
 
 
 level_1 = [
 "XXXXXXXXXXXXXXXXXXXXXXXXX",
-"XP XXXXXXX            XXX",
+"XP XXXXXXXE           XXX",
 "X  XXXXXXX   XXXXXX   XXX",
 "X            XXXXXX   XXX",
 "XXXXXXXXXX   XX S     XXX",
@@ -111,7 +164,7 @@ level_1 = [
 "X                XXXXXXXX",
 "X    XXXXXXXXX   XXXXXXXX",
 "X    XXXXXXXXX          X",
-"X         S             X",
+"X         S    E        X",
 "X    XXXXXXXXXXXXXX     X",
 "X    XXXXXXXXXXXXXX     X",
 "X         XXXXXXXXX     X",
@@ -121,7 +174,7 @@ level_1 = [
 "X         XXXXX  S XXXXXX",
 "XS        XXXXXXXXXXXXXXX",
 "XXXXX                XXXX",
-"XXXXXX               XXXX",
+"XXXXXX   E           XXXX",
 "XXXXXXXXXXXXXXXXX    XXXX",
 "XXXXXXXXXXXXXXXXX    XXXX",
 "X S   XXXXXXXXXXX    XXXX",
@@ -130,7 +183,10 @@ level_1 = [
 ]
 
 
-treasures = []
+sparkles = []
+
+
+enemies = []
 
 
 levels.append(level_1)
@@ -158,9 +214,13 @@ def setup_maze(level):
                     if character == "P":
                              player.goto(screen_x, screen_y)
 
-                             
+
                     if character == "S":
                          sparkles.append(Sparkle(screen_x, screen_y))
+
+
+                    if character == "E":
+                         enemies.append(Enemy(screen_x, screen_y))
 
 
 pen = Pen()
@@ -168,12 +228,10 @@ player = Player()
 
 
 walls = []
-sparkles = []
 
 
 setup_maze(levels[1])
 print (walls)
-
 
 turtle.listen()
 turtle.onkey(player.go_left,"Left")
@@ -183,6 +241,10 @@ turtle.onkey(player.go_down,"Down")
 
 
 wn.tracer(0)
+
+
+for enemy in enemies:
+     turtle.ontimer(enemy.move, t=250)
 
 
 while True:
@@ -198,6 +260,10 @@ while True:
 
                sparkles.remove(sparkle)
 
+
+     for enemy in enemies:
+          if player.is_collision(enemy):
+               print("Player dies!")
 
 
      wn.update()
